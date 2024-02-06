a81f4d74686a3fbd9d974d7efc93ebb493cb8ce3
jkrz-hein-bs <hein.htet.z25@bliss-stock.com>
2024-02-06 23:10:05 +0630

    form vue

---
+++ 003_reaction_timer/src/components/Results.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>Reaction Time: {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ResultTime",
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "ninja Finger";
+    } else if (this.score < 400) {
+      this.rank = "Rapid";
+    } else {
+      this.rank = "snail pace...";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4rem;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
